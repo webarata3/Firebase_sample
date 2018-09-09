ef186dffdbf5b5f86ccb7210816b6941a2412250
webarata3 <webarata3@gmail.com>
2018-09-09 09:18:51 +0900

    auth-gmailを使うように変更

--- public/storage/storage.js
+++ public/storage/storage.js
@@ -1,43 +1,5 @@
 'use strict';
 
-const loaderArea = document.getElementById('loaderArea');
-const loginButton = document.getElementById('loginButton');
-const logoutButton = document.getElementById('logoutButton');
-const displayName = document.getElementById('displayName');
-const profileImage = document.getElementById('profileImage');
-
-const provider = new firebase.auth.GoogleAuthProvider();
-
-loginButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  loaderArea.classList.remove('hidden');
-  firebase.auth().signInWithRedirect(provider);
-});
-
-logoutButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  loaderArea.classList.remove('hidden');
-  firebase.auth().signOut();
-});
-
-const PROFILE_PLACEHOLDER_IMAGE = '/image/profile_placeholder.png';
-
-firebase.auth().onAuthStateChanged((user) => {
-  loaderArea.classList.add('hidden');
-  if (user) {
-    loginButton.setAttribute('hidden', 'true');
-    logoutButton.removeAttribute('hidden');
-    displayName.textContent = user.displayName;
-    const profileImageUrl = user.photoURL || PROFILE_PLACEHOLDER_IMAGE;
-    profileImage.style.backgroundImage = `url(${profileImageUrl})`;
-  } else {
-    loginButton.removeAttribute('hidden');
-    logoutButton.setAttribute('hidden', 'true');
-    displayName.textContent = '';
-    profileImage.style.backgroundImage = 'none';
-  }
-});
-
 const fileButton = document.getElementById('file');
 const uploadStatusEl = document.getElementById('uploadStatus');
 const uploadFileNameEl = document.getElementById('uploadFileName');
